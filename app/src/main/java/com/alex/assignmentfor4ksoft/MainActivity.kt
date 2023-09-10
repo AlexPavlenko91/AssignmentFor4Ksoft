e0034d0175f494d3f9185201becc62071893e815
Oleksii Pavlenko <year1022@gmail.com>
2023-09-10 17:10:35 +0300

    initial commit

---
+++ app/src/main/java/com/alex/assignmentfor4ksoft/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.alex.assignmentfor4ksoft
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.alex.assignmentfor4ksoft.ui.theme.AssignmentFor4KsoftTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AssignmentFor4KsoftTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AssignmentFor4KsoftTheme {
+        Greeting("Android")
+    }
+}
